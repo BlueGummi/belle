8069536b36d9ae3f57a8d6c98a17d50f3264ca71
BlueGummi <thatstealthyowl@gmail.com>
2024-12-23 19:49:54 -0600

    update site

--- site/astro.config.mjs
+++ site/astro.config.mjs
@@ -13,23 +13,32 @@ export default defineConfig({
                     label: 'Program Documentation',
                     items: [
                         { label: 'Overview', slug: '' },
-			            { label: 'Assembler', slug: 'basm'},
-			            { label: 'Emulator', slug: 'belle'},
-			            { label: 'Diassembler', slug: 'bdump'},
-			            { label: 'Utilities', slug: 'btils'},
+                        { label: 'Assembler', slug: 'basm' },
+                        { label: 'Emulator', slug: 'belle' },
+                        { label: 'Disassembler', slug: 'bdump' },
+                        { label: 'Utilities', slug: 'btils' },
                     ],
                 },
                 {
                     label: 'ISA and hardware',
                     items: [
-                        { label: 'Overview', slug: 'overview'},
-                        { label: 'Encoding', slug: 'encoding'},
-                        { label: 'Instructions', slug: 'instructions'},
-			{ label: 'CPU specification', slug: 'cpu-core'},
-			{ label: 'Memory', slug: 'memory'},
+                        { label: 'Overview', slug: 'overview' },
+                        { label: 'Encoding', slug: 'encoding' },
+                        { label: 'Instructions', slug: 'instructions' },
+                        { label: 'CPU specification', slug: 'cpu-core' },
+                        { label: 'Memory', slug: 'memory' },
                     ],
                 },
             ],
         }),
     ],
+    devOptions: {
+        proxy: {
+            '/api': {
+                target: 'https://belle-demo.vercel.app',
+                changeOrigin: true,
+                pathRewrite: { '^/api': '/api' },
+            },
+        },
+    },
 });
