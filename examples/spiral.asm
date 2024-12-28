0133b2c5ff992d907865dbcf31f0724b12524300
BlueGummi <105593618+BlueGummi@users.noreply.github.com>
2024-12-28 16:51:45 -0600

    spiral.asm

--- examples/spiral.asm
+++ examples/spiral.asm
@@ -1,27 +1,49 @@
     ; This program increments the number being printed to stdout, then detects an overflow, before decrementing it again
     ; It is indefinite
     ; used for benchmarking
-.ssp [99]
-.sbp [99]
-.start [100]
+.ssp [3]
+.sbp [3]
+.start [4]
+    jmp @start
+string1:
+    .asciiz "Adding..."
+    .word 10
+string2:
+    .asciiz "Subtracting..."
+    .word 10
+start:
     mov r5, 31
     mov r4, 7
     mul r5, r4 ; 31 x 7 is in r5
     mov r4, 100
     add r4, 51 ; 151
     mul r5, r4
-    jmp @loop
-loop:
-    pop r4 ; pop off return address
-    add r0, 1
-    int 0
-    cmp r0, r5
-    je @sub_loop
-    jmp @loop
+    mul r5, 2
+    mov r4, 0
+    jmp @add_print
+add_loop:
+    pop r6 ; pop off return address
+    add r4, 1
+    cmp r4, r5
+    je @sub_print
+    jmp @add_loop
 sub_loop:
-    pop r4 ; pop off that return address
-    add r0, -1
-    int 0
-    cmp r0, r1
-    jz @loop
+    pop r6 ; pop off that return address
+    add r4, -1
+    cmp r4, r3
+    je @add_print
     jmp @sub_loop
+add_print:
+    pop r6
+    mov r0, @string1
+    mov r1, @string2
+    int 8
+    jmp @add_loop
+sub_print:
+    pop r6
+    mov r0, @string2
+    mov r1, @start
+    int 8
+    jmp @sub_loop
+
+
