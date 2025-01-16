43e579530dac8cbd5e7197410b5bde37e5ba2975
gummi <thatstealthyowl@gmail.com>
2025-01-15 18:24:27 -0600

    Move install scritps

---
+++ examples/binstall.ps1
@@ -0,0 +1,34 @@
+$zipUrl = "https://github.com/BlueGummi/belle/releases/download/nightly/belle-nightly-windows-x86_64.zip"
+$zipFile = "$env:TEMP\belle-nightly-windows-x86_64.zip"
+$extractPath = "$env:TEMP\belle"
+$localBinPath = "$env:USERPROFILE\.local\bin"
+$exeFiles = @("belle.exe", "basm.exe", "bdump.exe", "bfmt.exe")
+
+if (-not (Test-Path $localBinPath)) {
+    New-Item -ItemType Directory -Path $localBinPath
+}
+
+Invoke-WebRequest -Uri $zipUrl -OutFile $zipFile
+
+Expand-Archive -Path $zipFile -DestinationPath $extractPath -Force
+
+foreach ($exe in $exeFiles) {
+    $sourcePath = Join-Path -Path $extractPath -ChildPath "windows-bin\$exe"
+    $destinationPath = Join-Path -Path $localBinPath -ChildPath $exe
+
+    if (Test-Path $sourcePath) {
+        Move-Item -Path $sourcePath -Destination $destinationPath -Force
+    }
+}
+
+$pathEnv = [System.Environment]::GetEnvironmentVariable("PATH")
+if ($pathEnv -notlike "*$localBinPath*") {
+    [System.Environment]::SetEnvironmentVariable("PATH", "$pathEnv;$localBinPath", [System.EnvironmentVariableTarget]::User)
+}
+
+Remove-Item -Path $zipFile -Force
+
+Remove-Item -Path (Join-Path -Path $extractPath -ChildPath "windows-bin\*.exe") -Force
+
+Write-Host "Installed."
+Write-Host "Restart your terminal"
---
+++ examples/binstall.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+URL="https://github.com/BlueGummi/belle/releases/download/nightly/belle-nightly-linux-x86_64.tar.gz"
+TAR_FILE="belle-nightly-linux-x86_64.tar.gz"
+INSTALL_DIR="$HOME/.local/bin"
+
+if [ ! -d "$INSTALL_DIR" ]; then
+    mkdir -p "$INSTALL_DIR"
+fi
+
+curl -L -o "$TAR_FILE" "$URL"
+
+tar -xzf "$TAR_FILE"
+
+mv bin/basm "$INSTALL_DIR/bsam"
+mv bin/bdump "$INSTALL_DIR/bdump"
+mv bin/belle "$INSTALL_DIR/belle"
+mv bin/bfmt "$INSTALL_DIR/bfmt"
+
+rm -rf belle-nightly
+rm "$TAR_FILE"
+
+if ! echo "$PATH" | grep -q "$INSTALL_DIR"; then
+    if [[ "$SHELL" == *"bash"* ]]; then
+        echo "export PATH=\"$INSTALL_DIR:\$PATH\"" >> ~/.bashrc
+    elif [[ "$SHELL" == *"zsh"* ]]; then
+        echo "export PATH=\"$INSTALL_DIR:\$PATH\"" >> ~/.zshrc
+    elif [[ "$SHELL" == *"fish"* ]]; then
+        echo "set -gx PATH \"$INSTALL_DIR\" \$PATH" >> ~/.config/fish/config.fish
+    fi
+    echo "Please restart your terminal or run 'source ~/.bashrc' or 'source ~/.zshrc' or 'source ~/.config/fish/config.fish' to refresh your PATH."
+fi
+
+echo "Installed."
