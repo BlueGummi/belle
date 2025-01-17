60cce5d6624110d14eed52aaf19d9bda6e26b866
gummi <thatstealthyowl@gmail.com>
2025-01-17 07:18:43 -0600

    Update example script

--- examples/binstall.ps1
+++ examples/binstall.ps1
@@ -24,11 +24,20 @@ foreach ($exe in $exeFiles) {
 $pathEnv = [System.Environment]::GetEnvironmentVariable("PATH")
 if ($pathEnv -notlike "*$localBinPath*") {
     [System.Environment]::SetEnvironmentVariable("PATH", "$pathEnv;$localBinPath", [System.EnvironmentVariableTarget]::User)
+    Write-Host "Restart your terminal"
 }
 
 Remove-Item -Path $zipFile -Force
 
 Remove-Item -Path (Join-Path -Path $extractPath -ChildPath "windows-bin\*.exe") -Force
 
+Move-Item -Path "windows-bin/examples" -Destination "." -Recurse -Force
+
+Remove-Item -Path "windows-bin" -Recurse -Force
+
+Set-Location -Path "examples"
+
+ls
+
 Write-Host "Installed."
-Write-Host "Restart your terminal"
+Write-Host "Run 'make' in this directory to compile examples programs and execute them"
