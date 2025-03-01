3c3f1e08be6cfea02a6303b59342f1c141734e82
BlueGummi <thatstealthyowl@gmail.com>
2025-03-01 17:58:03 -0600

    comment

--- belle/src/hardware/core.rs
+++ belle/src/hardware/core.rs
@@ -200,11 +200,12 @@ impl CPU {
                     }
                     cycles += 1;
                 }
+                /*
                 println!(
                     "We took {:?} to execute {} instructions",
                     starting.elapsed(),
                     cycles
-                );
+                );*/
                 if CONFIG.pretty {
                     self_clone.pmem = !CONFIG.no_print_memory;
                     println!("{self_clone}");
